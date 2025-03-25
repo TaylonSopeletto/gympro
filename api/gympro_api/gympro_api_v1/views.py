10b5f16286702177c9a95066da8da1e443a4f576
TaylonSopeletto <gtataylon@gmail.com>
2025-03-24 23:32:20 -0300

    add post, put for exercises

--- api/gympro_api/gympro_api_v1/views.py
+++ api/gympro_api/gympro_api_v1/views.py
@@ -1,9 +1,17 @@
+from rest_framework import permissions, viewsets, generics, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 from django.contrib.auth.models import Group, User
+
 from .models import Teacher, Student, Exercise, Serie
-from rest_framework import permissions, viewsets, generics, status
-from gympro_api.gympro_api_v1.serializers import  UserSerializer, TeacherSerializer, StudentSerializer, ExerciseSerializer, SerieSerializer
+from gympro_api.gympro_api_v1.serializers import (
+    UserSerializer,
+    TeacherSerializer,
+    StudentSerializer,
+    ExerciseSerializer,
+    SerieSerializer,
+)
 
 class ExerciseList(generics.GenericAPIView):
     serializer_class = ExerciseSerializer
@@ -14,12 +22,22 @@ class ExerciseList(generics.GenericAPIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = ExerciseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+        new_exercise = Exercise.objects.create()
+        new_exercise.name = request.data['name']
+        new_exercise.weekday = request.data['weekday']
+        new_exercise.category = request.data['category']
+
+        new_exercise.save()
+
+        for serie in request.data["series"]:
+          new_serie = Serie.objects.create(weight=serie['weight'], repetitions=serie['repetitions'])
+          new_serie.save()
+          new_exercise.series.add(new_serie)
+
+        new_exercise.save()
+
+        serializer = ExerciseSerializer(new_exercise)
+        return Response(serializer.data, status=status.HTTP_201_CREATED) 
 
 class ExerciseDetail(generics.GenericAPIView):
     serializer_class = ExerciseSerializer
@@ -30,11 +48,59 @@ class ExerciseDetail(generics.GenericAPIView):
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        serializer = ExerciseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+        exercise = Exercise.objects.get(pk=pk) 
+        exercise.name = request.data['name']
+        exercise.weekday = request.data['weekday']
+        exercise.category = request.data['category']
+
+        exercise.series.clear()
+
+        for serie in request.data["series"]:
+            new_serie = Serie.objects.create(weight=serie['weight'], repetitions=serie['repetitions'])
+            new_serie.save()
+            exercise.series.add(new_serie)
+
+        exercise.save()
+
+        serializer = ExerciseSerializer(exercise)
+        return Response(serializer.data) 
+
+
+# better way of doing it
+
+# from django.db import transaction
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Exercise, Serie
+# from gympro_api.gympro_api_v1.serializers import ExerciseSerializer
+
+# class ExerciseCreateView(APIView):
+#     def post(self, request, format=None):
+#         with transaction.atomic():  # Ensure atomicity
+#             # Extract data safely
+#             name = request.data.get("name", "")
+#             weekday = request.data.get("weekday", "")
+#             category = request.data.get("category", "")
+#             series_data = request.data.get("series", [])
+
+#             # Create the exercise instance
+#             new_exercise = Exercise.objects.create(
+#                 name=name, weekday=weekday, category=category
+#             )
+
+#             # Bulk create series
+#             series_instances = [
+#                 Serie(weight=serie.get('weight', 0), repetitions=serie.get('repetitions', 0))
+#                 for serie in series_data
+#             ]
+#             created_series = Serie.objects.bulk_create(series_instances)
+
+#             # Add the created series to the exercise
+#             new_exercise.series.add(*created_series)
+
+#             # Serialize and return the response
+#             serializer = ExerciseSerializer(new_exercise)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     
