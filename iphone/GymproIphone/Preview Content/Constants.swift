cf125582e4cdfd5bd10d563beab9def45a4c4aec
TaylonSopeletto <gtataylon@gmail.com>
2025-03-31 19:46:48 -0300

    iphone: add header

---
+++ iphone/GymproIphone/Preview Content/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GymproIphone
+//
+//  Created by Taylon Sopeletto on 29/03/25.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = "http://127.0.0.1:8000"
+}
+
---
+++ iphone/GymproIphone/Shared/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  GymproIphone
+//
+//  Created by Taylon Sopeletto on 31/03/25.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
