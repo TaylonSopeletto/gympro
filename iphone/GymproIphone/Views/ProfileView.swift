dc359e6ca259fa3e46c334a43c8422e3e86a7af7
TaylonSopeletto <gtataylon@gmail.com>
2025-03-31 21:36:20 -0300

    iphone: add profile

---
+++ iphone/GymproIphone/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  GymproIphone
+//
+//  Created by Taylon Sopeletto on 31/03/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
