3c501ec785e52f2c1fdd3e247ce44628458f3ba6
TaylonSopeletto <gtataylon@gmail.com>
2025-04-05 20:27:12 -0300

    api: custom jwt token

---
+++ iphone/GymproIphone/Views/Teacher/TeacherView.swift
@@ -0,0 +1,76 @@
+//
+//  TeacherView.swift
+//  GymproIphone
+//
+//  Created by Taylon Sopeletto on 03/04/25.
+//
+
+import SwiftUI
+
+struct TeacherView: View {
+    
+    @State private var responseStudents: [StudentsResponse] = []
+    @State private var responseMessage = ""
+    
+    func getStudents() {
+        StudentsService.shared.get() { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let students):
+                    responseStudents = students
+                    print(students)
+                    
+                case .failure(let error):
+                    self.responseMessage = "Error: \(error.localizedDescription)"
+                }
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Header(title: "Teacher area", subtitle: "students")
+            Spacer()
+            HStack{
+                Text("Students")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 20)
+            VStack {
+                ForEach(responseStudents, id: \.full_name) { student in
+                    HStack {
+                        Text(student.full_name.capitalized)
+                            .padding()
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 70)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .foregroundColor(.black)
+                }
+            }
+            HStack{
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "plus.circle")
+                }
+                .font(.title)
+                .foregroundColor(.black)
+            }
+            .padding(.horizontal)
+            .padding(.top, 10)
+            Spacer()
+        }.onAppear(){
+            getStudents()
+        }
+       
+    }
+}
+
+#Preview {
+    TeacherView()
+}
