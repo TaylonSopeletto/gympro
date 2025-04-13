e005b8b1dbea3ebafe4f12427c555ca0726c7bf8
TaylonSopeletto <gtataylon@gmail.com>
2025-04-12 21:08:46 -0300

    equipament recognition: initial implemantation

---
+++ iphone/GymproIphone/Views/Student/AIView.swift
@@ -0,0 +1,18 @@
+//
+//  AIView.swift
+//  GymproIphone
+//
+//  Created by Taylon Sopeletto on 09/04/25.
+//
+
+import SwiftUI
+
+struct AIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AIView()
+}
