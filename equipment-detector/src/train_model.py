ae10742ef1efaf09bf0404f77f38653a317caac2
TaylonSopeletto <gtataylon@gmail.com>
2025-04-26 11:45:08 -0300

    equipement detector: organize folder structure

--- equipment-detector/src/train_model.py
+++ equipment-detector/src/train_model.py
@@ -58,7 +58,7 @@ model.fit(
 test_loss, test_acc = model.evaluate(validation_generator)
 print(f"Test accuracy: {test_acc}")
 
-img_path = 'celta.jpg'
+img_path = ''
 img = tf.keras.preprocessing.image.load_img(img_path, target_size=(150, 150))
 img_array = tf.keras.preprocessing.image.img_to_array(img) / 255.0
 img_array = tf.expand_dims(img_array, axis=0)
